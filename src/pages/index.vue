c4b325216d31a3de6aeb5e15686267f5e3c95243
Daniel Ahn <et3rnal.d@gmail.com>
2022-08-10 10:34:51 -0400

    work

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,6 +6,14 @@ const breakpoints = useBreakpoints(breakpointsTailwind)
 
 const smAndLarger = breakpoints.greater('sm')
 const mobile = breakpoints.smaller('sm')
+
+const isFormSubmitted = ref(false)
+
+const cardholderName = ref(null)
+const cardNumber = ref(null)
+const month = ref(null)
+const year = ref(null)
+const cvc = ref(null)
 </script>
 
 <template>
@@ -69,16 +77,16 @@ const mobile = breakpoints.smaller('sm')
     <!-- end card images for desktop -->
 
     <!-- form -->
-    <Form tracking-widest mt-30 class="sm:ml-[58%] md:ml-[51%] lg:ml-[42%] xl:ml-[34%] md:mr-[3%] lg:mr-[15%] xl:mr-[20%]">
+    <Form v-if="!isFormSubmitted" tracking-widest mt-30 class="sm:ml-[58%] md:ml-[51%] lg:ml-[42%] xl:ml-[34%] md:mr-[3%] lg:mr-[15%] xl:mr-[20%]" @submit="handleSubmit">
       <p text-xs font-bold mb-2>
         CARDHOLDER NAME
       </p>
-      <Field name="cardholder-name" mb-5 border-1 border-gray-400 px-4 py-2 rounded-lg type="text" class="w-[100%]" placeholder="e.g. Jane Appleseed" />
+      <Field v-model="cardholderName" name="cardholder-name" mb-5 border-1 border-gray-400 px-4 py-2 rounded-lg type="text" class="w-[100%]" placeholder="e.g. Jane Appleseed" />
 
       <p text-xs font-bold mb-2>
         CARD NUMBER
       </p>
-      <Field name="card-number" type="number" class="w-[100%]" mb-5 border-1 border-gray-400 px-4 py-2 rounded-lg placeholder="e.g. 1234 5678 9123 0000" />
+      <Field v-model="cardNumber" name="card-number" type="number" class="w-[100%]" mb-5 border-1 border-gray-400 px-4 py-2 rounded-lg placeholder="e.g. 1234 5678 9123 0000" />
       <!-- flexbox bottom row -->
       <div flex gap-3 mb-7>
         <div class="basis-1/2">
@@ -86,24 +94,45 @@ const mobile = breakpoints.smaller('sm')
             EXP. DATE (MM/YY)
           </p>
           <div flex gap-2>
-            <Field name="month" type="number" class="w-[50%]" placeholder="MM" border-1 border-gray-400 px-4 py-2 rounded-lg />
-            <Field name="year" type="number" class="w-[50%]" placeholder="YY" border-1 border-gray-400 px-4 py-2 rounded-lg />
+            <Field v-model="month" name="month" type="number" class="w-[50%]" placeholder="MM" border-1 border-gray-400 px-4 sm:px-3 md:px-4 py-2 rounded-lg />
+            <Field v-model="year" name="year" type="number" class="w-[50%]" placeholder="YY" border-1 border-gray-400 px-4 py-2 rounded-lg />
           </div>
         </div>
         <div class="basis-1/2" self-end>
           <p text-xs font-bold mb-2>
             CVC
           </p>
-          <Field name="cvc" type="number" w-full placeholder="e.g. 123" border-1 border-gray-400 px-4 py-2 rounded-lg />
+          <Field v-model="cvc" name="cvc" type="number" w-full placeholder="e.g. 123" border-1 border-gray-400 px-4 py-2 rounded-lg />
         </div>
       </div>
 
       <div>
-        <button type="submit" w-full rounded-lg bg-black text-white py-4>
+        <button type="submit" w-full rounded-lg dark:bg-violet-600 bg-black text-white py-4>
           Confirm
         </button>
       </div>
     </Form>
+
+    <!-- successful submission screen -->
+    <div v-if="isFormSubmitted" mt-30 class="sm:ml-[58%] md:ml-[51%] lg:ml-[42%] xl:ml-[34%] md:mr-[3%] lg:mr-[15%] xl:mr-[20%]">
+      <div mb-8>
+        <img mx-auto src="/icon-complete.svg">
+      </div>
+
+      <div text-center>
+        <h2 mb-4 text-3xl font-bold tracking-widest>
+          THANK YOU!
+        </h2>
+        <p mb-8 text-gray-500 text-sm>
+          We've added your card details
+        </p>
+      </div>
+      <div>
+        <button w-full rounded-lg dark:bg-violet-600 bg-black text-white py-4 @click="isFormSubmitted = !isFormSubmitted">
+          Continue
+        </button>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -118,6 +147,18 @@ const mobile = breakpoints.smaller('sm')
   background-image: url('/bg-main-desktop.png');
   background-repeat: no-repeat;
   background-size: contain;
+}
+
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
 }
 </style>
 
